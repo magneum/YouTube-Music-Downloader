1ac1022c94c6637dbcf7f7ef53cd98170d97c7fc
hypevoidsoul <hypevoidsoul@yahoo.com>
2021-07-10 07:28:46 -0700

    done!

--- ꜱᴇᴇᴅʀ/classi.py
+++ ꜱᴇᴇᴅʀ/classi.py
@@ -1,18 +1,14 @@
 import os
 class Feros(object):
     LOGGER = True
-    # ENV = bool(os.environ.get("HEROKU", False))
-    # API_HASH = os.environ.get("API_HASH",
-    # None)
-    # APP_ID = os.environ.get("APP_ID",
-    # None)
-    # BOT_TOKEN = os.environ.get("BOT_TOKEN",
-    # None)
-    #HYTM_KEY = os.environ.get("HYTM_KEY")
-    API_HASH = "4b7731b0a6d8e15bef82863887feb293"
-    APP_ID = 6372795
-    BOT_TOKEN = "1669919636:AAHjnWscap3c2V9XwMZduFLtd5JC79uHXz4"
-    HYTM_KEY = "~HYPE-kjeGcnwydKSKpSc-VOID~"
+    ENV = bool(os.environ.get("HEROKU", False))
+    API_HASH = os.environ.get("API_HASH",
+    None)
+    APP_ID = os.environ.get("APP_ID",
+    None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN",
+    None)
+    HYTM_KEY = os.environ.get("HYTM_KEY")
 class YTGX(Feros):
     LOGGER = True
     
